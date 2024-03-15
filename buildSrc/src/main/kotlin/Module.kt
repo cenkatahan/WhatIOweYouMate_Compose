10e8bec8d81f4c10c6dd31ef46a262be264a44ae
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-15 14:11:59 +0300

    feat: add modules
    Refs: Ticket-2

--- buildSrc/src/main/kotlin/Module.kt
+++ buildSrc/src/main/kotlin/Module.kt
@@ -1,2 +1,10 @@
 object Module {
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val home = ":feature:home"
+    const val add = ":feature:add"
+    const val update = ":feature:update"
+    const val summary = ":feature:summary"
 }
