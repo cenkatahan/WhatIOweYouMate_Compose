9f6069bd620824caccd7db9cf06e4ea57b249e60
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-17 17:19:27 +0300

    feat: add dependency: compose
    Refs: Ticket-4

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -31,6 +31,17 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 kapt {
