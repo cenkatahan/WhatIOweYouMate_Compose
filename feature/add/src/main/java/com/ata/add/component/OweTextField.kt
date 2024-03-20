43c5083cc97c0a0485fb390acc51ef9771516cb3
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-20 11:41:19 +0300

    feat: add AddScreen UI
    Refs: Ticket-17

---
+++ feature/add/src/main/java/com/ata/add/component/OweTextField.kt
@@ -0,0 +1,46 @@
+package com.ata.add.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Cancel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ata.core.ui.theme.OweGreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OweTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    label: @Composable() (() -> Unit),
+    onValueChange: (String) -> Unit,
+    leadingIcon: @Composable() (() -> Unit),
+    clearValue: () -> Unit,
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = {onValueChange(it)},
+        leadingIcon = leadingIcon,
+        trailingIcon = {
+            IconButton(onClick = { clearValue() }) {
+                Icon(
+                    imageVector = Icons.Rounded.Cancel,
+                    contentDescription = null,
+                    tint = OweGreen.color()
+                )
+            }
+        },
+        label = label,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = OweGreen.color(),
+            focusedBorderColor = OweGreen.color(),
+            unfocusedBorderColor = OweGreen.color()
+        )
+    )
+}
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //move to buildSrc and core
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
