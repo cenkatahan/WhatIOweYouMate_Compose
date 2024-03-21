12eda94d53bd11543ca13f5698f3de12ed1e1742
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-22 01:24:05 +0300

    feat: add GetFriedUseCase. Refs: Ticket-9

--- feature/update/src/main/java/com/ata/update/UpdateViewModel.kt
+++ feature/update/src/main/java/com/ata/update/UpdateViewModel.kt
@@ -3,6 +3,7 @@ package com.ata.update
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ata.core.data.datasource.local.entity.Friend
+import com.ata.core.domain.usecase.GetFriendUseCase
 import com.ata.core.domain.usecase.UpdateFriendUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -10,7 +11,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UpdateViewModel @Inject constructor(
-    private val updateFriendUseCase: UpdateFriendUseCase
+    private val updateFriendUseCase: UpdateFriendUseCase,
 ) : ViewModel() {
     fun update(friend: Friend) {
         viewModelScope.launch {
