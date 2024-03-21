66697c2e60207800d50c8973f9bd497f4c4c3e6c
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-22 01:00:52 +0300

    feat: add room related classes.
    Refs: Ticket-9

---
+++ core/src/main/java/com/ata/core/util/TableConstants.kt
@@ -0,0 +1,9 @@
+package com.ata.core.util
+
+object TableConstants {
+    const val TABLE_FRIEND = "friend_table"
+    const val COL_ID = "id"
+    const val COL_NAME = "name"
+    const val COL_PAYMENT = "payment"
+    const val KEY_ARGUMENT = "friend"
+}
