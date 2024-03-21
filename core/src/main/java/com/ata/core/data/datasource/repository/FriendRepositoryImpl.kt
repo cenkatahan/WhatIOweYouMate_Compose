89bf67f5491cfdafcae8ee1ca4a1bd9c5ceea053
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-22 01:01:53 +0300

    feat: add repository.
    Refs: Ticket-9

---
+++ core/src/main/java/com/ata/core/data/datasource/repository/FriendRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.ata.core.data.datasource.repository
+
+import com.ata.core.data.datasource.local.dao.FriendDao
+import com.ata.core.data.datasource.local.entity.Friend
+import com.ata.core.domain.repository.FriendRepository
+import javax.inject.Inject
+
+class FriendRepositoryImpl @Inject constructor(
+    private val dao: FriendDao
+) : FriendRepository {
+    override fun add(friend: Friend) = dao.save(friend)
+    override fun remove(friend: Friend) = dao.delete(friend)
+    override fun update(friend: Friend) = dao.update(friend)
+    override fun getFriendBy(id: Int): Friend = dao.getFriend(id)
+    override fun getFriends(): List<Friend> = dao.getFriends()
+    override fun removeFriends() = dao.deleteAll()
+
+}
