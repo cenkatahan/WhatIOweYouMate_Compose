fb487ea9b1253ef8a43d7bf70723f9d3527eefd8
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-22 01:11:28 +0300

    feat: add UpdateViewModel to UpdateScreen
    Refs: Ticket-9

--- feature/update/src/main/java/com/ata/update/UpdateViewModel.kt
+++ feature/update/src/main/java/com/ata/update/UpdateViewModel.kt
@@ -1,22 +1,21 @@
-package com.ata.add
+package com.ata.update
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ata.core.data.datasource.local.entity.Friend
-import com.ata.core.domain.usecase.SaveFriendUseCase
+import com.ata.core.domain.usecase.UpdateFriendUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddViewModel @Inject constructor(
-    private val saveFriendUseCase: SaveFriendUseCase
-): ViewModel() {
-    //todo add state.?
-    suspend fun save(friend: Friend){
+class UpdateViewModel @Inject constructor(
+    private val updateFriendUseCase: UpdateFriendUseCase
+) : ViewModel() {
+    fun update(friend: Friend) {
         viewModelScope.launch {
             try {
-                saveFriendUseCase.invoke(friend)
+                updateFriendUseCase.invoke(friend)
                 //state = Success
             } catch (e: Exception) {
                 //sate = Error
