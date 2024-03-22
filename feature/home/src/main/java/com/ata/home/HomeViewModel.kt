02abaef3ec826d9b8e728391a3f7ae39351f4be3
cenk atahan özbek <atahanozzbek@gmail.com>
2024-03-22 15:28:40 +0300

    feat: add fetch friends Refs: Ticket-9

--- feature/home/src/main/java/com/ata/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/ata/home/HomeViewModel.kt
@@ -8,6 +8,9 @@ import com.ata.core.domain.usecase.RemoveFriendsUseCase
 import com.ata.core.domain.usecase.RemoveUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import com.ata.home.HomeUIState.Loading
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,15 +21,30 @@ class HomeViewModel @Inject constructor(
     private val removeFriendsUseCase: RemoveFriendsUseCase
 ) : ViewModel() {
 
+    init {
+        fetchFriends()
+    }
+
+    private val _friendsState: MutableStateFlow<HomeUIState<List<Friend>>> =
+        MutableStateFlow(HomeUIState.Loading)
+    val friendsState = _friendsState.asStateFlow()
+
     fun remove(friend: Friend) {
         viewModelScope.launch(Dispatchers.Default) {
             removeUseCase.invoke(friend)
         }
     }
 
-    fun fetchFriends() = viewModelScope.launch(Dispatchers.Default) {
-        getFriendsUseCase.invoke().collect{
-            //add state
+    private fun fetchFriends() = viewModelScope.launch(Dispatchers.Default) {
+        try {
+
+            getFriendsUseCase.invoke().collect {
+                //add state
+                _friendsState.value = HomeUIState.Success(it)
+                _friendsState.value = HomeUIState.Success(listOf(Friend(name = "ATAHAN")))
+            }
+        } catch (e: Exception) {
+            _friendsState.value = HomeUIState.Error(e.message!!)
         }
     }
 
